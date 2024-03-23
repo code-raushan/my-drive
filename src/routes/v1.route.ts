ad41310426ead7e6bd7d6202db522a2942d77a61
Raushan Kumar <raushan.development@gmail.com>
2024-03-24 01:17:34 +0530

    add: file upload controllers added

--- src/routes/v1.route.ts
+++ src/routes/v1.route.ts
@@ -3,6 +3,7 @@ import { health, helloWorld } from '../controllers/health.controller';
 import { sendOTP, verifyOTP } from '../controllers/user.controller';
 import isLoggedIn from '../middlewares/auth.middleware';
 import { asyncHandler } from '../utils/asynchandler';
+import fileRouter from './file.route';
 
 const v1Router = Router();
 
@@ -10,5 +11,6 @@ v1Router.get('/', asyncHandler(helloWorld));
 v1Router.get('/health', isLoggedIn, asyncHandler(health));
 v1Router.post('/send-otp', asyncHandler(sendOTP));
 v1Router.post('/verify-otp', asyncHandler(verifyOTP));
+v1Router.use('/upload', fileRouter);
 
 export default v1Router;
