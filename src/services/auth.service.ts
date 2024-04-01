bbdb8934491ebae0a79e54a0107d35e77869455a
code-raushan <code-raushan@users.noreply.github.com>
2024-04-01 23:57:20 +0530

    update: removed unnecessary code and comments

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -20,28 +20,6 @@ class AuthService {
   }
 
   async sendOtp(phoneNumber: string) {
-    //   try {
-    //     const cognitoClient = await awsUtil.cognitoClient();
-    //     // check if user exists in cognito user pool
-    //     await this.checkUserExistenceInCognito(phoneNumber);
-
-    //     // Initiate custom authentication flow to send OTP
-    //     const params: InitiateAuthCommandInput = {
-    //       AuthFlow: "CUSTOM_AUTH",
-    //       ClientId: this.clientId,
-    //       AuthParameters: {
-    //         USERNAME: phoneNumber,
-    //       },
-    //     };
-
-    //     const input = new InitiateAuthCommand(params);
-    //     const response = await cognitoClient.send(input);
-    //     return response;
-    //   } catch (error) {
-    //     logger.error(`Error in send otp function - ${error}`);
-    //     throw error;
-    //   }
-
     try {
       const user = await this.userRepository.checkIfUserAlreadyExists(phoneNumber);
       const otp = generateOTP();
@@ -102,26 +80,7 @@ class AuthService {
 
   async verifyOtp(params: { phoneNumber: string, code: string, session: string }) {
     const { code, phoneNumber, session } = params;
-    // try {
-    //   const cognitoClient = await awsUtil.cognitoClient();
 
-    //   const params: RespondToAuthChallengeCommandInput = {
-    //     ChallengeName: "CUSTOM_CHALLENGE",
-    //     ClientId: this.clientId,
-    //     ChallengeResponses: {
-    //       USERNAME: phoneNumber,
-    //       ANSWER: code,
-    //     },
-    //     Session: session,
-    //   };
-
-    //   const input = new RespondToAuthChallengeCommand(params);
-    //   const response = await cognitoClient.send(input);
-    //   return response;
-    // } catch (error) {
-    //   logger.error(`Error verifying OTP - ${error}`);
-    //   throw error;
-    // }
     const userSession = await this._userSessionRepository.getSession({ phoneNumber, session });
     if (!userSession) throw new BadRequestError("Session not found, login again or create an account");
 
@@ -137,23 +96,6 @@ class AuthService {
   }
 
   async refreshToken(refreshToken: string) {
-    // try {
-    //   const cognitoClient = await awsUtil.cognitoClient();
-
-    //   const params: InitiateAuthCommandInput = {
-    //     AuthFlow: "REFRESH_TOKEN_AUTH",
-    //     ClientId: this.clientId,
-    //     AuthParameters: {
-    //       "REFRESH_TOKEN": refreshToken
-    //     }
-    //   };
-    //   const response = await cognitoClient.send(new InitiateAuthCommand(params));
-
-    //   return response;
-    // } catch (error) {
-    //   logger.error(`Error refreshing token - ${error}`);
-    //   throw error;
-    // }
     try {
       const payload = await verifyJWT(refreshToken, config.CLIENT_JWT_SECRET) as jwt.JwtPayload;
 
@@ -167,78 +109,6 @@ class AuthService {
       logger.error(error);
       throw new BadRequestError("Failed to refresh token");
     }
-  }
-
-  private async checkUserExistenceInCognito(phoneNumber: string) {
-    try {
-      const cognitoClient = await awsUtil.cognitoClient();
-
-      const { Username } = await cognitoClient.adminGetUser({
-        UserPoolId: this.userPoolId,
-        Username: phoneNumber,
-      });
-      if (!(await this.userRepository.checkIfUserAlreadyExists(phoneNumber))) {
-        const user = await this.userRepository.create(phoneNumber);
-        if (!user) throw new BadRequestError("Error creating user in database");
-
-        await cognitoClient.adminUpdateUserAttributes(
-          {
-            UserAttributes: [
-              {
-                Name: "custom:userId",
-                Value: user.id,
-              }
-            ],
-            UserPoolId: this.userPoolId,
-            Username: phoneNumber,
-          }
-        );
-      }
-      return Username;
-      //eslint-disable-next-line
-    } catch (error: any) {
-      if (error.__type === "UserNotFoundException") {
-        await this.createUserInCognitoAndDatabase(phoneNumber);
-      }
-      else {
-        throw error;
-      }
-    }
-  }
-
-  private async createUserInCognitoAndDatabase(phoneNumber: string) {
-    const cognitoClient = await awsUtil.cognitoClient();
-
-    let user = await this.userRepository.checkIfUserAlreadyExists(phoneNumber);
-    // Create the user in the database
-    if (!user) {
-      user = await this.userRepository.create(phoneNumber);
-    }
-
-    if (!user) throw new BadRequestError("Error creating user");
-
-    // Create the user in Cognito
-    await cognitoClient.adminCreateUser({
-      UserPoolId: this.userPoolId,
-      Username: phoneNumber,
-      UserAttributes: [
-        {
-          Name: "phone_number",
-          Value: phoneNumber,
-        },
-        {
-          Name: "phone_number_verified",
-          Value: "true",
-        },
-        {
-          Name: "custom:userId",
-          Value: user.id,
-        }
-      ],
-      TemporaryPassword: config.TEMPORARY_COGNITO_USER_PASSSWORD,
-      MessageAction: "SUPPRESS",
-    });
-
   }
 }
 
